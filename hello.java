d1eb7ad32b2b6810f4f494e57abb230e0dbf932f
CharlyGeen <charlygeen2@gmail.com>
2014-02-01 15:28:37 +0100

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello BBB world!");
+    }
+}
